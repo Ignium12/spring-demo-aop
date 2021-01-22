2784c76e4cf9251e6c5da7d8bec6319ae41c55fb
Ignium12 <kevin.ruehl12@googlemail.com>
2021-01-22 11:42:05 +0100

    Added new main program AroundDemoApp.class and TrafficFortuneService

---
+++ src/main/java/com/kruehl/aopdemo/AroundDemoApp.java
@@ -0,0 +1,28 @@
+package com.kruehl.aopdemo;
+
+import com.kruehl.aopdemo.service.TrafficFortuneService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AroundDemoApp {
+
+    public static void main(String[] args) {
+
+        // read spring config java class
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get the account bean from the spring container
+        TrafficFortuneService trafficFortuneService = context.getBean("trafficFortuneService", TrafficFortuneService.class);
+
+        System.out.println("\nMain Program: AroundDemoApp");
+        System.out.println("Calling getFortune");
+
+        String data = trafficFortuneService.getFortune();
+
+        System.out.println("\nMy fortune is: " +data);
+        System.out.println("Finished");
+
+        // close the context
+        context.close();
+    }
+}
